1b32d399372dd630555baeaa32dc318db9482433
julia0926 <kj980926@naver.com>
2023-03-08 10:26:02 +0900

    feat : CleanSwift 초기 세팅 및 메인 화면 구성

---
+++ BookApp/Source/Model/Book.swift
@@ -0,0 +1,12 @@
+//
+//  Book.swift
+//  BookApp
+//
+//  Created by Julia on 2023/03/08.
+//
+
+import Foundation
+
+struct Book {
+    
+}
---
+++ BookApp/Source/Service/BooksAPI.swift
@@ -0,0 +1,8 @@
+//
+//  BooksAPI.swift
+//  BookApp
+//
+//  Created by Julia on 2023/03/08.
+//
+
+import Foundation
