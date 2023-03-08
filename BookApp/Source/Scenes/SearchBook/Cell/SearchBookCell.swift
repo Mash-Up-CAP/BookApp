1cf63435b2c96125ac711506336cd1fba35b2e69
julia0926 <kj980926@naver.com>
2023-03-08 10:26:02 +0900

    feat : CleanSwift 초기 세팅 및 메인 화면 구성

---
+++ BookApp/Source/Scenes/SearchBook/Cell/SearchBookCell.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBookCell.swift
+//  BookApp
+//
+//  Created by Julia on 2023/03/07.
+//
+
+import UIKit
+
+final class SearchBookCell: UITableViewCell {
+    
+    @IBOutlet private weak var thumbnailImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var authorLabel: UILabel!
+    @IBOutlet private weak var publicationDateLabel: UILabel!
+    
+    func configure() {
+        
+    }
+}
